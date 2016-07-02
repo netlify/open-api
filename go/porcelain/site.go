12efd08c57bac0a38959e41d6218e80438259d2b
David Calavera <david.calavera@gmail.com>
2016-07-02 16:36:34 -0700

    Add ListSites porcelain.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- go/porcelain/site.go
+++ go/porcelain/site.go
@@ -6,6 +6,15 @@ import (
 	"github.com/netlify/open-api/go/porcelain/context"
 )
 
+func (n *Netlify) ListSites(ctx context.Context, params *operations.ListSitesParams) ([]*models.Site, error) {
+	resp, err := n.Netlify.Operations.ListSites(params, context.GetAuthInfo(ctx))
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
+}
+
 // GetSite returns a site.
 func (n *Netlify) GetSite(ctx context.Context, siteID string) (*models.Site, error) {
 	authInfo := context.GetAuthInfo(ctx)
