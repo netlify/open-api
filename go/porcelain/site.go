16dd5c7e51ff04ceb92fcae1125cbaf93380a57b
David Calavera <david.calavera@gmail.com>
2016-07-08 20:38:19 -0700

    Add porcelain method to create a site.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- go/porcelain/site.go
+++ go/porcelain/site.go
@@ -6,6 +6,7 @@ import (
 	"github.com/netlify/open-api/go/porcelain/context"
 )
 
+// List the sites a user has access to.
 func (n *Netlify) ListSites(ctx context.Context, params *operations.ListSitesParams) ([]*models.Site, error) {
 	resp, err := n.Netlify.Operations.ListSites(params, context.GetAuthInfo(ctx))
 	if err != nil {
@@ -19,6 +20,17 @@ func (n *Netlify) ListSites(ctx context.Context, params *operations.ListSitesPar
 func (n *Netlify) GetSite(ctx context.Context, siteID string) (*models.Site, error) {
 	authInfo := context.GetAuthInfo(ctx)
 	resp, err := n.Netlify.Operations.GetSite(operations.NewGetSiteParams().WithSiteID(siteID), authInfo)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Payload, nil
+}
+
+// Create a new site.
+func (n *Netlify) CreateSite(ctx context.Context, site *models.Site) (*models.Site, error) {
+	authInfo := context.GetAuthInfo(ctx)
+
+	resp, err := n.Netlify.Operations.CreateSite(operations.NewCreateSiteParams().WithSite(site), authInfo)
 	if err != nil {
 		return nil, err
 	}
