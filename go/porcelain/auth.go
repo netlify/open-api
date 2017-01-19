1e78ee324c5e1946d80b8297847a2cbdab3658dc
David Calavera <david.calavera@gmail.com>
2017-01-19 20:12:59 +0100

    Add a function to wait until an auth ticket is authorized by the server.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- go/porcelain/auth.go
+++ go/porcelain/auth.go
@@ -1,9 +1,17 @@
 package porcelain
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/Sirupsen/logrus"
 	"github.com/netlify/open-api/go/models"
 	"github.com/netlify/open-api/go/plumbing/operations"
 	"github.com/netlify/open-api/go/porcelain/context"
+)
+
+const (
+	ticketingTimeout = time.Minute * 5
 )
 
 // Create a login ticket to authenticate a user
@@ -38,4 +46,34 @@ func (n *Netlify) ExchangeTicket(ctx context.Context, ticketID string) (*models.
 	}
 
 	return resp.Payload, nil
+}
+
+func (n *Netlify) WaitUntilTicketAuthorized(ctx context.Context, ticket *models.Ticket) (*models.Ticket, error) {
+	authInfo := context.GetAuthInfo(ctx)
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
+
+	params := operations.NewShowTicketParams().WithTicketID(ticket.ID)
+	start := time.Now()
+	for t := range ticker.C {
+		resp, err := n.Netlify.Operations.ShowTicket(params, authInfo)
+		if err != nil {
+			time.Sleep(3 * time.Second)
+			continue
+		}
+		context.GetLogger(ctx).WithFields(logrus.Fields{
+			"ticket_id":  ticket.ID,
+			"authorized": resp.Payload.Authorized,
+		}).Debug("Waiting until deploy ready")
+
+		if resp.Payload.Authorized {
+			return resp.Payload, nil
+		}
+
+		if t.Sub(start) > ticketingTimeout {
+			return nil, fmt.Errorf("Error: the authorization process timed out")
+		}
+	}
+
+	return ticket, nil
 }
