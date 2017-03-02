3fe195bd0e74b1b014cc5a37d948add871d52750
David Calavera <david.calavera@gmail.com>
2017-03-01 20:20:54 -0800

    Add porcelain function to upload site assets.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- go/porcelain/assets.go
+++ go/porcelain/assets.go
@@ -1,11 +1,95 @@
 package porcelain
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/netlify/open-api/go/models"
 	"github.com/netlify/open-api/go/plumbing/operations"
 	"github.com/netlify/open-api/go/porcelain/context"
 )
+
+type SiteAsset struct {
+	SiteID  string
+	Name    string
+	Size    int64
+	Private bool
+	Body    io.ReadSeeker
+}
+
+func (n *Netlify) UploadNewSiteAsset(ctx context.Context, asset *SiteAsset) (*models.Asset, error) {
+	buffer := make([]byte, 512)
+	b, err := asset.Body.Read(buffer)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	contentType := http.DetectContentType(buffer[:b])
+	params := operations.NewCreateSiteAssetParams().WithSiteID(asset.SiteID).
+		WithName(asset.Name).WithSize(asset.Size).WithContentType(contentType)
+
+	if asset.Private {
+		visibility := "private"
+		params = params.WithVisibility(&visibility)
+	}
+
+	signature, err := n.AddSiteAsset(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	asset.Body.Seek(0, 0)
+	bufferWriter := &bytes.Buffer{}
+	writer := multipart.NewWriter(bufferWriter)
+
+	for key, value := range signature.Form.Fields {
+		writer.WriteField(key, value)
+	}
+
+	part, err := writer.CreateFormFile("file", asset.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = io.Copy(part, asset.Body); err != nil {
+		return nil, err
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", signature.Form.URL, bufferWriter)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 201 {
+		defer resp.Body.Close()
+		respBody, _ := ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("unexpected error uploading assets to the :cloud: %d - %s", resp.StatusCode, respBody)
+	}
+
+	updateParams := operations.NewUpdateSiteAssetParams().WithSiteID(asset.SiteID).WithAssetID(signature.Asset.ID).WithState("uploaded")
+
+	ar, err := n.UpdateSiteAsset(ctx, updateParams)
+	if err != nil {
+		return nil, err
+	}
+
+	return ar, nil
+}
 
 func (n *Netlify) AddSiteAsset(ctx context.Context, params *operations.CreateSiteAssetParams) (*models.AssetSignature, error) {
 	l := context.GetLogger(ctx)
