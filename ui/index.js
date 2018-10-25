0083c9b50e0c88df2c46cc0d363eb31d1ad01ae0
Bret Comnes <bcomnes@gmail.com>
2018-10-25 10:29:32 -0700

    Fix anchor links
    Closes https://github.com/netlify/open-api/issues/117

--- ui/index.js
+++ ui/index.js
@@ -3,5 +3,6 @@ const SwaggerUI = require('swagger-ui')
 SwaggerUI({
   dom_id: '#container',
   url: '/swagger.json',
-  displayOperationId: true
+  displayOperationId: true,
+  deepLinking: true
 })
