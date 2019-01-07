f27b426f71fbecce5b2f821eddcc2658e0ff1ccb
David Calavera <david.calavera@gmail.com>
2019-01-07 12:59:19 -0800

    Read original size from lfs pointers.
    So we can store that information.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- go/porcelain/deploy_test.go
+++ go/porcelain/deploy_test.go
@@ -6,23 +6,37 @@ import (
 )
 
 func TestGetLFSSha(t *testing.T) {
-	tests := []struct {
-		contents string
-		length   int
-	}{
-		{"Not a pointer file", 0},
-		{"version https://git-lfs.github.com/spec/v1\n" +
-			"oid sha256:7e56e498ccb4cbb9c672e1aed6710fb91b2fd314394a666c11c33b2059ea3d71\n" +
-			"size 1743570", 64},
-	}
-
-	for _, test := range tests {
-		file := strings.NewReader(test.contents)
-		out := getLFSSha(file)
-		if len(out) != test.length {
-			t.Fatalf("expected `%d`, got `%d`", test.length, len(out))
+	t.Run("test with not a pointer file", func(t *testing.T) {
+		file := strings.NewReader("Not a pointer file")
+		data, err := readLFSData(file)
+		if err != nil {
+			t.Fatal(err)
 		}
-	}
+
+		if data != nil {
+			t.Fatal("expected data to be nil without proper formatting")
+		}
+	})
+
+	t.Run("test with v1 pointer", func(t *testing.T) {
+		content := `version https://git-lfs.github.com/spec/v1
+oid sha256:7e56e498ccb4cbb9c672e1aed6710fb91b2fd314394a666c11c33b2059ea3d71
+size 1743570
+`
+		file := strings.NewReader(content)
+		data, err := readLFSData(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if data.SHA != "7e56e498ccb4cbb9c672e1aed6710fb91b2fd314394a666c11c33b2059ea3d71" {
+			t.Fatalf("expected `7e56e498ccb4cbb9c672e1aed6710fb91b2fd314394a666c11c33b2059ea3d71`, got `%v`", data.SHA)
+		}
+
+		if data.Size != 1743570 {
+			t.Fatalf("expected `1743570`, got `%v`", data.Size)
+		}
+	})
 }
 
 func TestAddWithLargeMedia(t *testing.T) {
