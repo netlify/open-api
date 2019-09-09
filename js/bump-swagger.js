58b565a0452926b7e5d9c4aa2e5bd81cca7cdbf0
ehmicky <ehmicky@gmail.com>
2019-09-09 16:28:11 +0200

    Refactor version bumping

--- js/bump-swagger.js
+++ js/bump-swagger.js
@@ -1,39 +1,24 @@
-const fs = require('fs')
-const path = require('path')
-const fromString = require('from2-string')
-const pump = require('pump')
-const split = require('split2')
-const pt = require('parallel-transform')
+const { readFile, writeFile } = require('fs')
+const { promisify } = require('util')
 
-const yamlPath = path.resolve(__dirname, '..', 'swagger.yml')
+const { version } = require('../package.json')
 
-const swaggerYaml = fs.readFileSync(yamlPath, 'utf8')
-const yamlStream = fromString(swaggerYaml)
-const writeStream = fs.createWriteStream(yamlPath)
+const pReadFile = promisify(readFile)
+const pWriteFile = promisify(writeFile)
 
-const newVersion = require('../package.json').version
-let oldVersion
-let didBumpVersion = false
+const SWAGGER_PATH = `${__dirname}/../swagger.yml`
 
-const splitString = 'version:'
+// Modify the `info.version` field inside `swagger.yml` so it matches the
+// `package.json` `version`
+const bumpSwagger = async function() {
+  const swaggerFile = await pReadFile(SWAGGER_PATH, 'utf8')
+  const newSwaggerFile = swaggerFile.replace(VERSION_REGEXP, `$1${version}`)
 
-function transform (chunk, cb) {
-  if (!didBumpVersion) {
-    if (chunk.includes(splitString)) {
-       const parts = chunk.split(splitString)
-       oldVersion = parts[1]
-       parts[1] = ' ' + newVersion
-       chunk = parts.join(splitString)
-       didBumpVersion = true
-    }
+  if (newSwaggerFile !== swaggerFile) {
+    await pWriteFile(SWAGGER_PATH, newSwaggerFile)
   }
-
-  chunk = chunk + '\n'
-  return cb(null, chunk)
 }
 
-pump(yamlStream, split(), pt(5, transform), writeStream, (err) => {
-  if (err) throw err
-  if (!didBumpVersion) throw new Error('error bumping swagger.yaml version: couldnt find version string ')
-  console.log('bumped swagger.yml from ' + oldVersion + ' to ' + newVersion)
-})
+const VERSION_REGEXP = /^(\s+version: )([\d.]+)$/m
+
+bumpSwagger()
