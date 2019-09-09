85eec3d1c94dc1fc04ab3134bb296f550f7e7093
ehmicky <ehmicky@gmail.com>
2019-09-09 18:42:11 +0200

    Enhance documentation UI

---
+++ redoc/build.js
@@ -0,0 +1,34 @@
+const { execFile } = require('child_process')
+const { promisify } = require('util')
+const { resolve } = require('path')
+
+const SWAGGER_PATH = resolve(__dirname, '..', 'swagger.yml')
+const OUTPUT_PATH = resolve(__dirname, 'dist', 'index.html')
+
+const pExecFile = promisify(execFile)
+
+// Build API documentation single self-contained HTML file using `redoc-cli`
+const buildDocs = async function() {
+  await pExecFile('redoc-cli', [
+    `--title=${TITLE}`,
+    `--options.expandResponses=${SUCCESS_STATUS_CODES}`,
+    '--options.requiredPropsFirst',
+    '--options.jsonSampleExpandLevel=1',
+    `--options.theme.colors.primary.main=${HEADINGS_TEXT_COLOR}`,
+    `--options.theme.menu.textColor=${MENU_TEXT_COLOR}`,
+    `--options.theme.menu.backgroundColor=${MENU_BACKGROUND_COLOR}`,
+    `--options.theme.typography.headings.fontFamily=${FONT}`,
+    `--output=${OUTPUT_PATH}`,
+    'bundle',
+    SWAGGER_PATH,
+  ])
+}
+
+const TITLE = 'Netlify API documentation'
+const SUCCESS_STATUS_CODES = [200, 201, 204].join(',')
+const HEADINGS_TEXT_COLOR = '#00c2b2'
+const MENU_TEXT_COLOR = '#8b8b8b'
+const MENU_BACKGROUND_COLOR = '#ffffff'
+const FONT = 'Roboto, sans-serif'
+
+buildDocs()
