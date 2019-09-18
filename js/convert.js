a67f141220aaea1fe59ef27c97086e8e49fc1a32
ehmicky <ehmicky@gmail.com>
2019-09-18 14:20:57 +0200

    Add comment

--- js/convert.js
+++ js/convert.js
@@ -10,6 +10,8 @@ const YAML_INPUT = `${__dirname}/../swagger.yml`
 const OUTPUT_DIR = `${__dirname}/dist`
 const JSON_OUTPUT = `${OUTPUT_DIR}/swagger.json`
 
+// Validate `swagger.yml`, dereference the JSON references then serialize to
+// `swagger.json`
 const convertOpenApi = async function() {
   const [openapiDef] = await Promise.all([
     SwaggerParser.validate(YAML_INPUT, { dereference: { circular: false } }),
