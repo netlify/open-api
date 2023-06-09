554c19c4dd11e782a7eed5450f69ad8aace56369
Sarah Etter <sarah@sarahetter.com>
2023-06-09 09:59:00 -0400

    feat: add feature_flags parameter to the getSite endpoint (#466)
    * feat: add feature_flags parameter to the getSite endpoint
    
    * add .netlify to gitignore
    
    * chore: testing x-internal on property
    
    * feat: upgrade to new version of redoc cli, move parameter to only getSite
    
    * feat: add filtering for x-internal
    
    * run: make all
    
    ---------
    
    Co-authored-by: Erik Westrup <erik.westrup@gmail.com>

--- src/docs/build.js
+++ src/docs/build.js
@@ -5,7 +5,7 @@ const execa = require('execa')
 const { copyAssets } = require('./assets')
 const { injectContent } = require('./inject')
 
-const SWAGGER_PATH = `${__dirname}/../../swagger.yml`
+const SWAGGER_PATH = `${__dirname}/../../external.yml`
 const OUTPUT_DIR = `${__dirname}/../../dist`
 const OUTPUT_PATH = `${OUTPUT_DIR}/index.html`
 
@@ -16,17 +16,17 @@ const buildDocs = async function () {
 
 const redocCli = async function () {
   await execa(
-    'redoc-cli',
+    'redocly',
     [
       `--title=${TITLE}`,
-      '--options.requiredPropsFirst',
-      '--options.sortOperationsAlphabetically',
-      `--options.theme.colors.primary.main=${HEADINGS_TEXT_COLOR}`,
-      `--options.theme.sidebar.backgroundColor=${MENU_BACKGROUND_COLOR}`,
-      `--options.theme.typography.headings.fontFamily=${FONT}`,
-      `--options.theme.logo.gutter=${LOGO_PADDING}`,
+      '--theme.openapi.requiredPropsFirst',
+      '--theme.openapi.sortOperationsAlphabetically',
+      `--theme.openapi.colors.primary.main=${HEADINGS_TEXT_COLOR}`,
+      `--theme.openapi.sidebar.backgroundColor=${MENU_BACKGROUND_COLOR}`,
+      `--theme.openapi.typography.headings.fontFamily=${FONT}`,
+      `--theme.openapi.logo.gutter=${LOGO_PADDING}`,
       `--output=${normalize(OUTPUT_PATH)}`,
-      'build',
+      'build-docs',
       SWAGGER_PATH,
     ],
     { stdio: 'inherit' }
