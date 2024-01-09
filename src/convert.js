68e80f23b379d5179a2ab82e12bf6544390d27d3
Angel Mendez <amendez1988@gmail.com>
2024-01-08 18:01:05 -0600

    fix: remove callback as is not need in fs/promises

--- src/convert.js
+++ src/convert.js
@@ -11,7 +11,7 @@ const JSON_OUTPUT = `${OUTPUT_DIR}/swagger.json`
 const convertOpenApi = async function () {
   const [openapiDef] = await Promise.all([
     SwaggerParser.validate(YAML_INPUT, { dereference: { circular: false } }),
-    mkdir(OUTPUT_DIR, { recursive: true }, () => {}),
+    mkdir(OUTPUT_DIR, { recursive: true }),
   ])
   const openapiJson = JSON.stringify(openapiDef, null, 2)
   await writeFile(JSON_OUTPUT, openapiJson)
