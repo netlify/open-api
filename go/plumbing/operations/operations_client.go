554414922ecb44cd8e246cee7aa6337156b56373
David Berlin <david@stackbit.com>
2024-06-09 12:22:53 +0300

    feat: Mark dev server activity

--- go/plumbing/operations/operations_client.go
+++ go/plumbing/operations/operations_client.go
@@ -225,6 +225,8 @@ type ClientService interface {
 
 	LockDeploy(params *LockDeployParams, authInfo runtime.ClientAuthInfoWriter) (*LockDeployOK, error)
 
+	MarkDevServerActivity(params *MarkDevServerActivityParams, authInfo runtime.ClientAuthInfoWriter) (*MarkDevServerActivityOK, error)
+
 	NotifyBuildStart(params *NotifyBuildStartParams, authInfo runtime.ClientAuthInfoWriter) (*NotifyBuildStartNoContent, error)
 
 	ProvisionSiteTLSCertificate(params *ProvisionSiteTLSCertificateParams, authInfo runtime.ClientAuthInfoWriter) (*ProvisionSiteTLSCertificateOK, error)
@@ -3660,6 +3662,41 @@ func (a *Client) LockDeploy(params *LockDeployParams, authInfo runtime.ClientAut
 	// unexpected success response
 	unexpectedSuccess := result.(*LockDeployDefault)
 	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+MarkDevServerActivity mark dev server activity API
+*/
+func (a *Client) MarkDevServerActivity(params *MarkDevServerActivityParams, authInfo runtime.ClientAuthInfoWriter) (*MarkDevServerActivityOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewMarkDevServerActivityParams()
+	}
+
+	result, err := a.transport.Submit(&runtime.ClientOperation{
+		ID:                 "markDevServerActivity",
+		Method:             "POST",
+		PathPattern:        "/sites/{site_id}/dev_servers/{dev_server_id}/activity",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"https"},
+		Params:             params,
+		Reader:             &MarkDevServerActivityReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*MarkDevServerActivityOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for markDevServerActivity: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
 }
 
 /*
